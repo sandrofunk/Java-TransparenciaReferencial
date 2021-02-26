e73f336d6554d9aea7e629d2e93cb3f5e59208e5
sandrofunk <sagfunk@gmail.com>
2021-02-26 14:58:17 -0300

    Projeto Criado

---
+++ src/Program.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Program {
+	
+	public static int globalValue = 3;
+
+	public static void main(String[] args) {
+		
+		int[] vect = new int[] {3, 4, 5};
+		changeOddValues(vect);
+		System.out.println(Arrays.toString(vect));
+	}
+	
+	//Soma o resto da equação com a varial globalValue de valor 3
+	public static void changeOddValues(int[] numbers) {
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] % 2 != 0) {
+				numbers[i] += globalValue;
+			}
+		}
+	}
+	
+
+}
